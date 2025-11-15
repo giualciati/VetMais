5ac7802111c6bbca73a58e3808fb2696d28beb31
Camis25 <camisvitoriarosasantos2005@gmail.com>
2025-11-15 00:43:14 -0300

    iniciando projeto com conexão ao banco mysql.

---
+++ Backend/src/main/java/br/com/vetmais/VetmaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.vetmais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetmaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetmaisApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/br/com/vetmais/VetmaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vetmais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetmaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
